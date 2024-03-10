3055d5166662b52782564c0d4501b8213d01f716
lukimann <lukewanyama5@gmail.com>
2024-03-10 09:49:47 +0300

    pushed

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,15 @@
 class Author:
-    pass
-
+    def __init__(self, name):
+        self.name = name
 
 class Book:
-    pass
+    def __init__(self, title):
+        if not isinstance(title, str):
+            raise TypeError("Title must be a string")
+        self.title = title
+
 
 
 class Contract:
     pass
+
